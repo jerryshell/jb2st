d1fc6cf7191fc9e71fed3f5395840bff6126a325
jerry <calljerryli@outlook.com>
2022-12-05 21:46:32 +0800

    update camelCase2SnakeCase()

--- src/util.ts
+++ src/util.ts
@@ -1,7 +1,7 @@
 const camelCase2SnakeCase = (camelCaseStr: string) => {
-  let snakeCaseStr = camelCaseStr.replace(/[A-Z]/g, (match) => '_' + match.toLowerCase())
+  const snakeCaseStr = camelCaseStr.replace(/[A-Z]/g, (match) => '_' + match.toLowerCase())
   if (snakeCaseStr.slice(0, 1) === '_') {
-    snakeCaseStr = snakeCaseStr.slice(1)
+    return snakeCaseStr.slice(1)
   }
   return snakeCaseStr
 }
